21767f5fceb301de5b09d0854e55e406d1c73e4a
Johannes <johannesharz@gmail.com>
2025-11-03 22:47:27 +0100

    Added Solutions bis 22

--- schueler.py
+++ schueler.py
@@ -1,27 +1,8 @@
 from framework.grundlage import level
-level.lade(104,weiblich=True)
+level.lade(0,weiblich=True)
 from framework.grundlage import *
 
 # Ab hier darfst du programmieren:
-
-
-knappe = held.gib_knappe()
-knappe.geh()
-monster = knappe.gib_objekt_vor_dir()
-
-t = monster.gib_objekt_vor_dir().gib_spruch()
-
-monster.rechts()
-monster.angriff()
-door = held.gib_objekt_vor_dir()
-
-door.spruch_anwenden(t)
-held.geh()
-held.geh()
-held.geh()
-held.nimm_herz()
-
-
 
 
 # Dieser Befehl muss immer am Ende stehen
